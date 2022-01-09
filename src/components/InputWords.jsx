a1750c1918435d9b806b7e3f37700870eb00004b
phanvuong <vuongpa@melisoft.vn>
2022-01-09 15:50:57 +0700

    vuong update

--- src/components/InputWords.jsx
+++ src/components/InputWords.jsx
@@ -1,49 +1,52 @@
 import { useEffect, useState } from "react";
 import { Button, Col, Row, Select } from "antd";
-import rawAdj from "../data/tinhtu.txt";
-import rawNoun from "../data/danhtu.txt";
-import rawVerb from "../data/dongtu.txt";
 import FetchData from "../data/FetchData";
 import { Point } from "./Constants";
 
-const findTypeOfWord = (word, danhTu, dongTu) => {
+// Import txt file 
+import TINH_TU from "../data/tinhtu.txt";
+import DANH_TU from "../data/danhtu.txt";
+import DONG_TU from "../data/dongtu.txt";
+
+const findWordInString = (word, stringType) => {
+    const position =  stringType.search(word);
+    if (position === -1) 
+        return false;
+    return true;
+}
+
+const findTypeOfWord = (word, danhTu, dongTu, tinhTu) => {
     let typeOfWord = {
         value: word,
         types: []
     };
-    let position = -1;
-    // Find in string danh tu
-    position = danhTu.search(word);
-    if (position !== -1) {
-        typeOfWord.types.push("DANH_TU");
-    }
-    // Find in string tinh tu
-    position = dongTu.search(word);
-    if (position !== -1) {
-        typeOfWord.types.push("DONG_TU");
-    }
-
-    // Find in string dong tu
+    findWordInString(word, danhTu) && typeOfWord.types.push("DANH_TU");
+    findWordInString(word, dongTu) && typeOfWord.types.push("DONG_TU");
+    findWordInString(word, tinhTu) && typeOfWord.types.push("TINH_TU");
     return typeOfWord;
 }
 
 const InputWords = () => {
-    const [noun, setNoun] = useState("");
-    const [verb, setVerb] = useState("");
     const [words, setWords] = useState([""]);
+
+    // useState words
+    const [dataWords, setDataWords] = useState([{}]);
+
     const handleSelectChange = (value) => {
         setWords(value);
     };
+
     const handleClick = () => {
         for (let i = 0; i < words.length; ++i) {
-            const typesOfWord = findTypeOfWord(words[i], noun, verb);
+            const typesOfWord = findTypeOfWord(words[i], danhTu, dongTu, tinhTu);
             console.log(typesOfWord);
         }
     }
 
     useEffect(() => {
-        FetchData(rawNoun).then(value => setNoun(value));
-        FetchData(rawVerb).then(value => setVerb(value));
+        FetchData(DANH_TU).then(value => setDataWords(prevState => );
+        FetchData(DONG_TU).then(value => setDongTu(value.toLocaleLowerCase()));
+        FetchData(TINH_TU).then(value => setTinhTu(value.toLocaleLowerCase()));
     }, [])
 
     return (
